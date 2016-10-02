8b4b62bca66f48660d2d63c34c024475c535ab19
guilhermeresende <guiresendeborges@yahoo.com>
2016-10-02 16:44:20 -0300

    Added sampling

--- crawler.py
+++ crawler.py
@@ -2,62 +2,86 @@ import urllib
 import urllib2
 import re
 import json
+import time
 
-users = ["guilhermeresende"]
 tokenf=open("token.txt")
 token=tokenf.read()
 tokenf.close()
+
+
+def create_req(url):
+	req=urllib2.Request(url)
+	req.add_header("Authorization","token "+token)
+	return req
+
+users = ["guilhermeleobas"]
+f=open("commits.txt","w")
+commitsha = set()
+
 for user in users:
 	repos=[]
 	url="https://api.github.com/users/"+user+"/repos"
-	req=urllib2.Request(url)
-	req.add_header("Authorization","token "+token)
-	response=urllib2.urlopen(req)
-
+	
+	response=urllib2.urlopen(create_req(url))
 	results= json.loads(response.read())
+
 	for i in results:
 		repos.append(i[u'commits_url'][:-6])
+
 	for repo in repos:
-		print repo
-		req=urllib2.Request(repo)
-		req.add_header("Authorization","token "+token)
+		numreqs=0
+		start = time.time()
+
+		print repo		
+		req=create_req(repo)
+		numreqs+=1 #TESTE
+
 		try:
 			response=urllib2.urlopen(req)
 			link=response.info().getheader('Link')
-			if(link==None):
+
+			if(link==None): #find number of pages
 				last=1
 			else:
 				m = re.search('rel="next",(.*)page=(\d+)>; rel="last"',link)
 				last=int(m.group(2))
 
 			currentpage=1
-			author_list=[]
-			numcommits=0
 			while(True):
-				l=response.read()
-				#print "read response"
-				commits=json.loads(l)
-				#print "json loaded"	
-				numcommits+=len(commits)	
-				for commit in commits:				
-					author_list.append(commit[u'commit'][u'author'][u'email'])
-				#print "analyzed"
+				commits=json.loads(response.read())
+	
+				for commit in commits:
+
+					if not commit[u'sha'] in commitsha: #writes commit
+						s=repo+"\t"+commit[u'commit'][u'author'][u'email']+"\t"+commit[u'commit'][u'author'][u'date']+"\n"
+						commitsha.add(commit[u'sha'])
+						f.write(s)
+
+						if commit[u'committer']!=None: #adds authors
+							if commit[u'committer'][u'login'] not in users:
+								users.append(commit[u'committer'][u'login'])
+								print users	
+
 				if(currentpage==last):
 					break
 				else:
 					currentpage+=1
 					url=repo+"?page="+str(currentpage)
-					req=urllib2.Request(url)
 					print url
-					req.add_header("Authorization","token "+token)
-					response=urllib2.urlopen(req)
-					#print "got response"
+					response=urllib2.urlopen(create_req(url))
+					numreqs+=1 #TESTE
+
+				
 		except urllib2.URLError as e:
 			if(e.reason=="Conflict"):
 				print "empty repository"
-				numcommits=0
-				author_list=[]
 			else:
 				raise
 
-		print "number of commits:",numcommits,"different authors:",len(set(author_list))
+		end = time.time()
+		reqs_per_s=float(numreqs)/(end-start)
+		print reqs_per_s
+		if(reqs_per_s>1.38):
+			time.sleep(1)
+
+f.close()
