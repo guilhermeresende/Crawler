6ac3013f04fe7d001219fddc110c9db4a9d082a5
guilhermeresende <guilherme.resende.borges@gmail.com>
2017-03-22 21:41:55 -0300

    New version

--- crawler.py
+++ crawler.py
@@ -28,7 +28,15 @@ def do_req(url):
 	except urllib2.HTTPError as e:
 		if e.code == 451:
 			print "Page not allowed"
-		return json.loads("[]")
+			return json.loads("[]")
+		elif e.code == 404:
+			print "page not found"
+			return json.loads("[]")
+		elif e.code == 403:
+			print "page forbidden"
+			return json.loads("[]")
+		else:
+			raise
 	return json.loads(response.read())
 
 def get_fork_info(url):
@@ -65,7 +73,6 @@ def collect_repos(url, repos,checked_repos):
 	while(True):
 		results=json.loads(response.read())
 		for res in results:
-			#print res[u'commits_url'], type(res[u'commits_url']), list(checked_repos)[0],type(list(checked_repos)[0])
 			if not(res[u'commits_url'][:-6] in checked_repos):
 				if(res[u'fork']==False): #gets repo_link, owner, and if its a fork, fork count
 					repos.append((res[u'commits_url'][:-6],res[u'owner'][u'login'],res[u'fork'],res[u'forks_count']))
@@ -88,7 +95,7 @@ def get_num_pages(link):
 		m = re.search('rel="next",(.*)page=(\d+)>; rel="last"',link)
 		return int(m.group(2))
 
-def collect_commits_from_push(results):
+def get_commits_from_push(results):
 	s=""
 	for res in results[u'commits']:
 		s+="commits"+"\t"+res[u'url']+"\t"+res[u'author'][u'email']+"\t"+res[u'author'][u'name']+"\n"
@@ -96,6 +103,7 @@ def collect_commits_from_push(results):
 
 def get_user_info(user):
 	url="https://api.github.com/users/"+user+"/events"
+	print url
 	req=create_req(url)
 	response=urllib2.urlopen(req)
 	
@@ -111,7 +119,7 @@ def get_user_info(user):
 			#fuser.write(s)
 
 			if res[u'type']=='PushEvent':
-				s+=collect_commits_from_push(res[u'payload'])
+				s+=get_commits_from_push(res[u'payload'])
 
 		if(currentpage==last):
 			break
@@ -128,6 +136,12 @@ def collect_commits_from_repo(req,repo,users,allusers,fcommit):
 		if e.code == 451:
 			print "page not allowed"
 			return
+		elif e.code == 404:
+			print "page not found"
+			return
+		elif e.code == 403:
+			print "page forbidden"
+			return
 		else:
 			raise
 	last = get_num_pages(response.info().getheader('Link'))	
@@ -143,7 +157,8 @@ def collect_commits_from_repo(req,repo,users,allusers,fcommit):
 				s+="\t"+str(commit[u'author'][u'id'])
 				if commit[u'author'][u'login'] not in allusers:
 					users.append(commit[u'author'][u'login'])
-					allusers.add(commit[u'author'][u'login'])	
+					allusers.add(commit[u'author'][u'login'])
+
 			s+="\n"
 			fcommit.write(s.encode('utf-8'))
 		if(currentpage==last):
@@ -152,19 +167,18 @@ def collect_commits_from_repo(req,repo,users,allusers,fcommit):
 			currentpage+=1
 			url=repo+"?page="+str(currentpage)
 			print url, last
-			response=urllib2.urlopen(create_req(url))
-
-			'''Tries to read and load untill it works'''
+			
+			'''Tries to read page untill it works'''
 			signal.signal(signal.SIGALRM, handler)
 			signal.alarm(10) 
 			readdone=1
 			while(readdone):
 				try:
 					readdone=0
+					response=urllib2.urlopen(create_req(url))					
 					commits=json.loads(response.read())	
-				except IOError as e:
+				except:
 					readdone=1
-					response=urllib2.urlopen(create_req(url))
 					signal.alarm(10)
 			signal.alarm(0)
 
@@ -176,103 +190,112 @@ def recover_from_fail():
 	flist=open("userlist.txt","r")
 	currentuser=flist.readline().strip("\n").strip("\r")
 	users=eval(flist.readline().strip("\n").strip("\r"))
-	fcommit=open("commits.txt","r")
+	#allusers=eval(flist.readline().strip("\n").strip("\r"))
+	#checked_repos=eval(flist.readline().strip("\n").strip("\r"))
+
+	current_repo='https://api.github.com/repos/mozilla/gecko-dev/commits'
+	fcommit = open("commits.txt","r+")
 	fuser=open("events.txt","r")
-	for line in fcommit:
+
+	commitlines = fcommit.readlines()
+	fcommit.seek(0)
+	
+	for line in commitlines:
 		fields=line.strip("\n").strip("\r").split("\t")
+		if fields[2]!=currentuser:
+			fcommit.write(line)
+		else:
+			break
 		if fields[0]=="repo_info:":
 			if not(fields[2] in allusers):
 				allusers.add(fields[2])
 			if not(fields[1] in checked_repos):
-				checked_repos.add(fields[1])
-
-		if fields[2]==currentuser:
-			break
-		commits+=line
-
+				checked_repos.add(fields[1])		
+	fcommit.truncate()
 	for line in fuser:
 		fields=line.strip("\n").strip("\r").split("\t")
 		if fields[0]==currentuser:
 			break
 		events+=line
 
-	for user in users:
-		allusers.add(user)
+	#for user in users:
+	#	allusers.add(user)
 	fcommit.close()
 	fuser.close()
 	flist.close()
-	return (currentuser,users,commits,events,allusers,checked_repos)
+	return (currentuser,users,events,allusers,checked_repos)
 
+def main():
+	global numreqs
+	if len(sys.argv)>1 and sys.argv[1]=='--restart': 	#start collecting from beginning
+		users = ["guilhermeresende"]
+		allusers=set()
+		checked_repos=set()
+		allusers.add("guilhermeresende")
+		fcommit=open("commits.txt","w")
+		fuser=open("events.txt","w")	
+	else: 												#start from last user
+		print "Recovering from last restart" 
+		(currentuser,users,events,allusers,checked_repos)=recover_from_fail()
+		fcommit=open("commits.txt","a+")
+		fcommit.seek(0,2)
+		fuser=open("events.txt","w")
+		#fcommit.write(commits)
+		fuser.write(events)
+		frst=users.index(currentuser)
+		users=users[frst:]
 
-if len(sys.argv)>1 and sys.argv[1]=='--restart':
-	users = ["guilhermeresende"]
-	allusers=set()
-	checked_repos=set()
-	allusers.add("guilhermeresende")
-	fcommit=open("commits.txt","w")
-	fuser=open("events.txt","w")	
-else:
-	(currentuser,users,commits,events,allusers,checked_repos)=recover_from_fail()
-	fcommit=open("commits.txt","w")
-	fuser=open("events.txt","w")
-	fcommit.write(commits)
-	fuser.write(events)
-	frst=users.index(currentuser)
-	users=users[frst:]
+	start = time.time()
+	for user in users:
+		print "User", user
+		flist=open("userlist.txt","w")
+		flist.write(user+"\n"+str(users)+"\n"+str(allusers)+"\n"+str(checked_repos))
+		flist.close()
+		repos=[]
+		url="https://api.github.com/users/"+user+"/repos"	
 
-start = time.time()
-for user in users:
-	print "User", user
-	flist=open("userlist.txt","w")
-	flist.write(user+"\n"+str(users))
-	flist.close()
-	repos=[]
-	url="https://api.github.com/users/"+user+"/repos"	
+		collect_repos(url,repos,checked_repos)
 
-	collect_repos(url,repos,checked_repos)
+		url="https://api.github.com/users/"+user+"/orgs"	
+		orgs=get_orgs(url)
 
-	s=get_user_info(user)
-	s=s.encode('utf-8')
-	fuser.write(s)
+		for org in orgs:
+			url=org[u'repos_url']
+			collect_repos(url,repos,checked_repos)	
 
-	url="https://api.github.com/users/"+user+"/orgs"	
-	orgs=get_orgs(url)
+		for repoinfo in repos:
+			repo=repoinfo[0]
+			print repo
 
-	for org in orgs:
-		url=org[u'repos_url']
-		collect_repos(url,repos,checked_repos)	
+			written_repoinfo="\t".join([str(partinfo).encode('utf-8') for partinfo in repoinfo])
+			fcommit.write("repo_info:\t"+written_repoinfo+"\n")
+					
+			req=create_req(repo)
+			
+			try:
+				collect_commits_from_repo(req,repo,users,allusers,fcommit)
+			except urllib2.URLError as e:
+				if(e.reason=="Conflict"):
+					print "empty repository"
+				else:
+					raise
 
-	for repoinfo in repos:
-		repo=repoinfo[0]
-		print repo
+			#controls for request limit
+			end = time.time()
+			time_dif=(end-start)
+			'''if(time_dif>3600 and numreqs<=4999):
+				numreqs=0
+				start=time.time()
+			elif(time_dif>3600 and numreqs>4999): 
+				print "WAIT FOR REQ LIMIT",3600-time_dif
+				time.sleep(3600-time_dif)
+				numreqs=0
+				start=time.time()'''
 
-		written_repoinfo="\t".join([str(partinfo).encode('utf-8') for partinfo in repoinfo])
-		fcommit.write("repo_info:\t"+written_repoinfo+"\n")
-				
-		req=create_req(repo)
-		
-		try:
-			collect_commits_from_repo(req,repo,users,allusers,fcommit)
-		except urllib2.URLError as e:
-			if(e.reason=="Conflict"):
-				print "empty repository"
-			else:
-				raise
+		print "Reqs and time", numreqs, time_dif, numreqs/time_dif
 
-		#controls for request limit
-		end = time.time()
-		time_dif=(end-start)
-		if(time_dif>3600):
-			numreqs=0
-			start=time.time()
-		elif(numreqs>=4999): 
-			print "WAIT FOR REQ LIMIT"
-			time.sleep(3600-time_dif)
-			numreqs=0
-			start=time.time()
+	f.close()
+	fuser.close()
 
-	#print "Proportion with user",float(wth_author)/(wth_author+wthout_author) #number of commits with author
-	print "Reqs and time", numreqs, time_dif, numreqs/time_dif
-
-f.close()
-fuser.close()
+if __name__ == "__main__":
+    main()
